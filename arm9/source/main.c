cb5bf8deabb9701176461105374bee4b0207370d
coto <programmer.coto.1988@gmail.com>
2020-01-10 18:17:10 -0300

    Update binary:
    - Disable VCOUNT Interrupts since these are unused and might generate sound clicks
    
    TGDS Changes:
    
    - Added proper VCOUNT Interrupts.

--- arm9/source/main.c
+++ arm9/source/main.c
@@ -932,7 +932,9 @@ int main(int _argc, sint8 **_argv) {
 	RenderTGDSLogoSubEngine((uint8*)&TGDSLogoLZSSCompressed[0], TGDSLogoLZSSCompressed_size);
 
 	//Init sound
+	DisableIrq(IRQ_VCOUNT);
 	disableVBlank();
+	//We keep HBLANK IRQs to let CPU sleep as long the PPU triggers IRQs
 	initComplexSound(); // initialize sound variables
 	
 	//Init TGDS FS Directory Iterator Context(s). Mandatory to init them like this!! Otherwise several functions won't work correctly.
