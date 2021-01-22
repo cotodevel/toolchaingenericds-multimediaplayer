27eb5a380bfe97bf6eb8d938dd50bdc7394cdf2e
Coto <programmer.coto.1988@gmail.com>
2021-01-22 16:52:58 +0000

    remove inlined interrupt handlers as the compiler may remove then, subsequently a segfault would occur.

--- arm7/source/interrupts/Interrupts_app7.c
+++ arm7/source/interrupts/Interrupts_app7.c
@@ -32,7 +32,6 @@ USA
 #ifdef ARM9
 __attribute__((section(".itcm")))
 #endif
-inline __attribute__((always_inline)) 
 void IpcSynchandlerUser(uint8 ipcByte){
 	switch(ipcByte){
 		default:{
@@ -45,7 +44,6 @@ void IpcSynchandlerUser(uint8 ipcByte){
 #ifdef ARM9
 __attribute__((section(".itcm")))
 #endif
-inline __attribute__((always_inline)) 
 void Timer0handlerUser(){
 
 }
@@ -53,7 +51,6 @@ void Timer0handlerUser(){
 #ifdef ARM9
 __attribute__((section(".itcm")))
 #endif
-inline __attribute__((always_inline)) 
 void Timer1handlerUser(){
 	
 }
@@ -61,21 +58,19 @@ void Timer1handlerUser(){
 #ifdef ARM9
 __attribute__((section(".itcm")))
 #endif
-inline __attribute__((always_inline)) 
 void Timer2handlerUser(){
 }
 
 #ifdef ARM9
 __attribute__((section(".itcm")))
 #endif
-inline __attribute__((always_inline)) 
 void Timer3handlerUser(){
+
 }
 
 #ifdef ARM9
 __attribute__((section(".itcm")))
 #endif
-inline __attribute__((always_inline)) 
 void HblankUser(){
 	
 }
@@ -83,7 +78,6 @@ void HblankUser(){
 #ifdef ARM9
 __attribute__((section(".itcm")))
 #endif
-inline __attribute__((always_inline)) 
 void VblankUser(){
 	
 }
@@ -91,7 +85,6 @@ void VblankUser(){
 #ifdef ARM9
 __attribute__((section(".itcm")))
 #endif
-inline __attribute__((always_inline)) 
 void VcounterUser(){
 
 }
@@ -100,7 +93,6 @@ void VcounterUser(){
 #ifdef ARM9
 __attribute__((section(".itcm")))
 #endif
-inline __attribute__((always_inline)) 
 void screenLidHasOpenedhandlerUser(){
 	setBacklight(POWMAN_BACKLIGHT_TOP_BIT | POWMAN_BACKLIGHT_BOTTOM_BIT);	//both lit screens
 	SetLedState(LED_ON);
@@ -111,7 +103,6 @@ void screenLidHasOpenedhandlerUser(){
 #ifdef ARM9
 __attribute__((section(".itcm")))
 #endif
-inline __attribute__((always_inline)) 
 void screenLidHasClosedhandlerUser(){
 	setBacklight(0);
 	SetLedState(LED_LONGBLINK);
