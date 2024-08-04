31bee295cbff4011f2be6dc4dabc067e07562153
cotodevel <programmer.coto.1988@gmail.com>
2024-08-04 04:08:58 -0400

    Restore support for M4A (AAC 192-bit) 3hours+ audio files.

--- arm9/source/TGDSMemoryAllocator/TGDSMemoryAllocator.c
+++ arm9/source/TGDSMemoryAllocator/TGDSMemoryAllocator.c
@@ -38,7 +38,7 @@ struct AllocatorInstance * getProjectSpecificMemoryAllocatorSetup(bool isCustomT
 	customMemoryAllocator->customMalloc = isCustomTGDSMalloc;
 	
 	customMemoryAllocator->ARM9MallocStartaddress = (u32)sbrk(0);
-	customMemoryAllocator->memoryToAllocate = (2000*1024);
+	customMemoryAllocator->memoryToAllocate = (2304*1024);
 	customMemoryAllocator->CustomTGDSMalloc9 = (TGDSARM9MallocHandler)&Xmalloc;
 	customMemoryAllocator->CustomTGDSCalloc9 = (TGDSARM9CallocHandler)&Xcalloc;
 	customMemoryAllocator->CustomTGDSFree9 = (TGDSARM9FreeHandler)&Xfree;
